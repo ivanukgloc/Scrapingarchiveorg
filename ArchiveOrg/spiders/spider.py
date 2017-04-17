727de74e0d7de00f7ba11eb721333e42ebcb3632
Ivan <ivan.ukgloc@gmail.com>
2017-04-17 05:50:45 +0300

    consider next link and total matches

--- ArchiveOrg/spiders/spider.py
+++ ArchiveOrg/spiders/spider.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
+import re
 
 from ArchiveOrg.items import ArchiveorgItem
 
@@ -10,15 +11,32 @@ is_empty = lambda x, y=None: x[0] if x else y
 class ArchiveSpider(scrapy.Spider):
     name = "archive_product"
     start_urls = ['https://archive.org/details/georgeblood?sort=-publicdate']
+    PAGE_LINK = 'https://archive.org/details/georgeblood?&sort=-publicdate&page={page_number}'
+    item_per_page = 75
 
     def start_requests(self):
-        yield scrapy.Request(url=self.start_urls[0], callback=self.parse_links)
+        yield scrapy.Request(url=self.start_urls[0], callback=self.parse_pages)
+
+    def parse_pages(self, response):
+        page_links = []
+        total_count = response.xpath('//div[@class="columns-facets"]'
+                                     '/h3/text()').extract()
+        total_count = self._clean_text(total_count[0])
+        total_count = re.search('\d+', total_count).group(0)
+        range_count = int(total_count) / self.item_per_page
+
+        for page_num in range(1, range_count):
+            link = self.PAGE_LINK.format(page_number=page_num)
+            page_links.append(link)
+
+        for page_link in page_links:
+            yield scrapy.Request(url=page_link, callback=self.parse_links, dont_filter=True)
 
     def parse_links(self, response):
         href_links = response.xpath('//div[contains(@class, "item-ttl")]'
                                     '/a/@href').extract()
         for href in href_links:
-            link = 'https://archive.org' + href
+            link = 'https://archive.org%s' % href
             yield scrapy.Request(url=link, callback=self.parse_product, dont_filter=True)
 
     def parse_product(self, response):
@@ -64,11 +82,15 @@ class ArchiveSpider(scrapy.Spider):
                                             '/a/@href').extract())
 
         if 'date' in more_link:
-            more_link = 'https://archive.org' + more_link
+            more_link = 'https://archive.org%s' % more_link
 
         else:
             more_link = "None"
 
         return more_link
 
+    def _clean_text(self, text):
+        text = re.sub("[\n\t]", "", text)
+        text = re.sub(",", "", text).strip()
+        return text
 
